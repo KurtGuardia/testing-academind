53b99e502210d9ac2db62f6311f6de1f31fbaeb7
Kurt Guardia <kurtguardia@gmail.com>
2021-03-30 14:32:19 +0200

    Integration test done

--- util.test.js
+++ util.test.js
@@ -1,5 +1,6 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
+//Unit Testing
 test('should output name and age', () => {
   const text = generateText('Kurt', 29);
   expect(text).toBe('Kurt (29 years old)');
@@ -7,9 +8,16 @@ test('should output name and age', () => {
   expect(text2).toBe('Anna (28 years old)');
 });
 
+//Against false positives
 test('should output dataless text', () => {
   const text = generateText('', null);
   expect(text).toBe(' (null years old)');
   const text2 = generateText();
   expect(text2).toBe('undefined (undefined years old)');
+});
+
+//Integrtion test
+test('should generate a valid text output', () => {
+  const text = checkAndGenerate('Max', 29);
+  expect(text).toBe('Max (29 years old)');
 });
